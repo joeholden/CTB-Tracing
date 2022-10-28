44f8a24e365d0895345913f84bd4bbf16a09f29e
joeholden <joemattholden@gmail.com>
2022-10-27 23:01:10 -0500

    Update get_thresholds.py
    made it insensitive to chosen LUT

--- get_thresholds.py
+++ get_thresholds.py
@@ -4,6 +4,11 @@ import os
 
 
 def get_threshold(image_path):
+    """
+    Takes in an 8-bit red LUT tif and allows user input to determine lower threshold
+    Outputs the thresholds for all images in a directory to an Excel sheet
+    Read in any LUT you want
+    """
     def dummy(x):
         pass
 
@@ -11,10 +16,13 @@ def get_threshold(image_path):
 
     original_rgb = cv2.imread(img_path)
     original_rgb = cv2.resize(original_rgb, (512, 512))
+    # Determine the LUT used
+    first_pixel = original_rgb[0, 0]
+    filled_channels = [i for i, e in enumerate(first_pixel) if e != 0]  # 0, 1, 2 for b, g, r
+
     for x in range(original_rgb.shape[1]):
         for y in range(original_rgb.shape[0]):
-            original_rgb[y, x] = [0, original_rgb[y, x][2],
-                                  0]  # todo: this assumes reading in a read image and makes green
+            original_rgb[y, x] = [0, original_rgb[y, x][filled_channels[0]], 0]
 
     cv2.namedWindow('SC Section')
     cv2.createTrackbar('Threshold', 'SC Section', 50, 255, dummy)  # 50 is the initialization value for the bar
@@ -22,8 +30,8 @@ def get_threshold(image_path):
     end = False
     pressed_left = False
     pressed_right = False
-    left = None
-    right = None
+    left_thresh = None
+    right_thresh = None
 
     while not end:
         rgb = original_rgb.copy()
@@ -37,7 +45,7 @@ def get_threshold(image_path):
         for x in range(rgb.shape[0]):
             for y in range(rgb.shape[1]):
                 if thresh[y, x][1] == 255:
-                    rgb[y, x] = [0, 0, 255]  # todo: this assumes reading in a read image and makes green
+                    rgb[y, x] = [0, 0, 255]
 
         cv2.imshow('SC Section', rgb)
 
@@ -45,16 +53,16 @@ def get_threshold(image_path):
         if k == 113:  # Left SC, key 'q'
             print(f'Left: {slider_val}')
             pressed_left = True
-            left = slider_val
+            left_thresh = slider_val
         if k == 119:  # Right SC, key 'w'
             print(f'Right {slider_val}')
             pressed_right = True
-            right = slider_val
+            right_thresh = slider_val
         if pressed_left and pressed_right:
             end = True
 
     cv2.destroyAllWindows()
-    return left, right
+    return left_thresh, right_thresh
 
 
 thresholds = {}
@@ -66,5 +74,5 @@ for root, dirs, files in os.walk(directory_with_images):
 filenames = thresholds.keys()
 left_thresholds = [i[1][0] for i in thresholds.items()]
 right_thresholds = [i[1][1] for i in thresholds.items()]
-df = pd.DataFrame(zip(filenames, left_thresholds, right_thresholds), columns=['filename', 'left', 'right'])
+df = pd.DataFrame(zip(filenames, left_thresholds, right_thresholds), columns=['Filename', 'Left BG', 'Right BG'])
 df.to_excel('threshold_values.xlsx')
